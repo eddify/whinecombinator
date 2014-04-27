dbb0b765cde2dbb90afbc83f89590e5339e55212
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-04-26 21:55:22 -0700

    add quick sort hacks

--- public/js/models/whine.js
+++ public/js/models/whine.js
@@ -6,6 +6,10 @@ App.Models.Whine = Backbone.Model.extend({
     var score = parseInt(this.get('score'));
     $.getJSON('/whines/'+this.get('_id')+'/upvote', function(data){
       this.set('score', score+1);
+
+      // quick hack
+      app.whinesView.collection.sort();
+      app.whinesView.render();
     }.bind(this));
   }
 });
