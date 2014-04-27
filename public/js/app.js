8c3635dcac7eca64ebd301a743e3ab504adfa683
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-04-26 19:55:55 -0700

    backbone time

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
-function WhineCtrl($scope) {
-  $scope.whines = [{
-    username: 'Kayvon Fatehi',
-    tweet: 'whine, bitch, complain, and more whining!'
-  }, {
-    username: 'Marcio Barbosa',
-    tweet: 'whine, whine, bitch, whine!'
-  }, {
-    username: 'Eddy Kim',
-    tweet: 'whine, whine, I dont whine too much!'
-  }];
-};
+var sample_whines = [{
+  username: 'Kayvon Fatehi',
+  tweet: 'whine, bitch, complain, and more whining!'
+}, {
+  username: 'Marcio Barbosa',
+  tweet: 'whine, whine, bitch, whine!'
+}, {
+  username: 'Eddy Kim',
+  tweet: 'whine, whine, I dont whine too much!'
+}];
 
+var WhinesCollection = Backbone.Collection.extend({
+  
+});
+
+var WhinesView = Backbone.Collection.extend({
+  
+});
+
+var view = new WhinesView();
+
+view.setElement('#whines');
