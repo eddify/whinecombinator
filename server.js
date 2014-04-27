f94e6ebed838220a694a67942127fc23db499696
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-04-26 18:22:22 -0700

    add console message on server run

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+var port = process.env.PORT || 1337;
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 1337);
+app.listen(port);
+console.log("Listening on port "+port);
 
