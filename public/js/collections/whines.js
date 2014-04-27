5208ea5464a8277e308459931d0cdee64e3db113
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-04-26 21:52:58 -0700

    add comparator

--- public/js/collections/whines.js
+++ public/js/collections/whines.js
@@ -1,5 +1,8 @@
 App.Collections.Whines = Backbone.Collection.extend({
   model: App.Models.Whine,
-  url: '/whines'
+  url: '/whines',
+  comparator: function(model) {
+    return -model.get('score');
+  }
 });
 
